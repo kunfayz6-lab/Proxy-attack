423d3cf7cdf3b77f4cc74df452d44b884f39d5ca
kunfayz6-lab <kunfayz6@gmail.com>
2025-10-22 00:29:00 +0700

    Update run.py

--- run.py
+++ run.py
@@ -35,7 +35,7 @@ def check_prox(array, url):
 
 while threading.active_count()>150 :
     time.sleep(5)
-Thread.start()
+Thread:{threading.get_ident()}
 def __init__(self):
         print("init")
 	
