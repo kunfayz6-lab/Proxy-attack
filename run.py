7d6202f89bc5915eb3f570e9a13452e29da0a747
kunfayz6-lab <kunfayz6@gmail.com>
2025-10-21 22:50:54 +0700

    Update run.py

--- run.py
+++ run.py
@@ -21,6 +21,17 @@ print( """
 \033[1;34m                                                          \033[0m          
 \033[1;34m    ## BRIGADE ATTACKER SNIPER ELITE ==> internal script By:ZA99\033[0m ##
 """)
+while attemps < 100:
+    username = input("\033[32mEnter your username: \033[0m")
+    password = input("\033[31mEnter your password: \033[0m")
+
+    if username == 'bp4' and password == 'bp4':
+        print("\033[32m⟩⟩ Hai...! Welcome to zona attack BLACKPHANTER \033[0m")
+        break
+    else:
+        print('Incorrect credentials. Check if you have Caps lock on and try again.')
+        attemps += 1
+        continue
 
 def check_prox(array, url):
 	ip = r.post("http://ip.beget.ru/").text
